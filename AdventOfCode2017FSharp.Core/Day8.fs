45f645d79b4d6038ccfe05455461590f35c17fa9
Paul Marfleet <paul.marfleet@gmail.com>
2018-08-31 14:44:29 +0100

    Added Day 8 (Part 1) parser + tests

---
+++ AdventOfCode2017FSharp.Core/Day8.fs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2017FSharp.Core
+
+module Day8 =
+
+    open System.Text.RegularExpressions
+
+    type Register = string
+
+    type AdjustmentType = Increase | Decrease with
+        static member Parse = function
+            | "inc" -> Increase
+            | "dec" -> Decrease
+            | _ -> failwith "Invalid adjustment type. Should be 'inc' or 'dec'." 
+
+    type Operator =
+        | GreaterThan
+        | LessThan
+        | GreaterThanOrEqual
+        | LessThanOrEqual
+        | Equals
+        | NotEquals with
+            static member Parse = function
+                | ">" -> GreaterThan
+                | ">=" -> GreaterThanOrEqual
+                | "<" -> LessThan
+                | "<=" -> LessThanOrEqual
+                | "==" -> Equals
+                | "!=" -> NotEquals
+                | _ -> failwith "Invalid operator. Should be '<', '<=', '>', '>=', '==', or '!='."
+
+    type Condition = { Register : Register;
+                       Operator : Operator;
+                       Amount : int }
+    type Instruction = { Register : Register;
+                         AdjustmentType : AdjustmentType;
+                         AdjustmentAmount : int;
+                         Condition : Condition }
+
+    let regex = new Regex("(?<reg>[a-z]+)\s(?<adj_type>inc|dec)\s(?<adj_amt>-?\d+)\sif\s(?<cond_reg>[a-z]+)\s(?<cond_op>\>|\<|\>=|\<=|==|!=)\s(?<cond_amt>-?\d+)")
+
+    let parseInstruction input =
+        let m = regex.Match(input)
+        { Register = m.Groups.["reg"].Value;
+          AdjustmentType = AdjustmentType.Parse m.Groups.["adj_type"].Value;
+          AdjustmentAmount = int m.Groups.["adj_amt"].Value;
+          Condition = { Register = m.Groups.["cond_reg"].Value;
+                        Operator = Operator.Parse m.Groups.["cond_op"].Value;
+                        Amount = int m.Groups.["cond_amt"].Value } }
---
+++ AdventOfCode2017FSharp.Tests/Day8.fs
@@ -0,0 +1,46 @@
+﻿namespace Day8
+    module Day8 =
+
+        open Xunit
+        open FsUnit.Xunit
+        open AdventOfCode2017FSharp.Core.Day8
+
+        type Tests () =
+
+            static member Instructions
+                with get() =
+                    [ [| "b inc 5 if a > 1" :> obj; 
+                         { Register = "b"; 
+                           AdjustmentType = Increase; 
+                           AdjustmentAmount = 5; 
+                           Condition = { Register = "a"; 
+                                         Operator = GreaterThan; 
+                                         Amount = 1 } } :> obj |];
+                      [| "a inc 1 if b < 5";
+                         { Register = "a";
+                           AdjustmentType = Increase;
+                           AdjustmentAmount = 1;
+                           Condition = { Register = "b";
+                                         Operator = LessThan;
+                                         Amount = 5 } } |];
+                      [| "c dec -10 if a >= 1";
+                         { Register = "c";
+                           AdjustmentType = Decrease;
+                           AdjustmentAmount = -10;
+                           Condition = { Register = "a";
+                                         Operator = GreaterThanOrEqual;
+                                         Amount = 1 } } |];
+                      [| "c inc -20 if c == 10";
+                         { Register = "c";
+                           AdjustmentType = Increase;
+                           AdjustmentAmount = -20;
+                           Condition = { Register = "c";
+                                         Operator = Equals;
+                                         Amount = 10 } } |] ]
+
+            [<Theory>]
+            [<MemberData("Instructions")>]
+            member verify.``Instruction can be parsed`` (input : string, expected : Instruction) =
+                parseInstruction input |> should equal expected
+
+    
