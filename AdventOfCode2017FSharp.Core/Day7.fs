7b381371249a0b17b5e8e86cd666d00d982cae13
Paul Marfleet <paul.marfleet@gmail.com>
2018-08-31 11:09:47 +0100

    Look as far up the tree as possible to find the program with the wrong weight.  Still not returning correct result for puzzle input.

--- AdventOfCode2017FSharp.Core/Day7.fs
+++ AdventOfCode2017FSharp.Core/Day7.fs
@@ -24,22 +24,28 @@ module Day7 =
                         |> Array.map (fun line -> (line.Name, line))
                         |> Map.ofArray
 
-    let calculateCorrectWeight (lines : Map<string, Line>) bottom =
-        let rec getWeight key =
+    let calculateCorrectWeight (lines : Map<string, Line>) bottomProgram =
+        let rec calculateCorrectWeight' key =
             let program = lines.[key]
-            let dependentWeights = program.Dependents |> Seq.sumBy (fun kv -> getWeight kv.Key)
-            program.Weight + dependentWeights
+            match program.Dependents.IsEmpty with
+                | true -> program.Weight
+                | false -> 
+                    let dependentWeights = program.Dependents 
+                                            |> Map.map (fun k _ -> calculateCorrectWeight' k)
+                    let groups = dependentWeights
+                                    |> Seq.groupBy (fun x -> x.Value)
+                                    |> Seq.sortBy (fun x -> snd x |> Seq.length)
+                                    |> Seq.toArray
+                    match groups.Length with
+                        | 1 -> program.Weight + (fst groups.[0] * (snd groups.[0] |> Seq.length))
+                        | _ ->
+                            let keyOfProgramToAdjust = ((snd groups.[0]) |> Seq.head).Key
+                            let programToAdjust = lines.[keyOfProgramToAdjust]
+                            let adjustment = fst groups.[1] - fst groups.[0]
 
-        let dependentWeights = bottom.Dependents |> Map.map (fun k _ -> getWeight k) 
-        let groups = dependentWeights
-                        |> Seq.groupBy (fun x -> x.Value)
-                        |> Seq.sortBy (fun x -> snd x |> Seq.length)
-                        |> Seq.toArray
-        let keyOfProgramToAdjust = ((snd groups.[0]) |> Seq.head).Key
-        let program = lines.[keyOfProgramToAdjust]
-        let adjustment = fst groups.[1] - fst groups.[0]
+                            programToAdjust.Weight + adjustment
 
-        program.Weight + adjustment
+        calculateCorrectWeight' bottomProgram.Name
 
     let calculatePart1 lines =
         let found = lines |> Map.findKey 
