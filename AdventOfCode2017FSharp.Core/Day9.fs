97c7e4f03d954007a58097812118afe0e34c415e
Paul Marfleet <paul.marfleet@gmail.com>
2018-09-15 23:33:45 +0100

    Rafactored Day 9 Part 1 solution to use Active Patterns

--- AdventOfCode2017FSharp.Core/Day9.fs
+++ AdventOfCode2017FSharp.Core/Day9.fs
@@ -9,29 +9,46 @@ module Day9 =
     type State = { Score : int;
                    Depth : int;
                    Mode : Mode }
+                   member this.HasFlag flag = this.Mode &&& flag = flag
+                   member this.SetFlag flag = this.Mode ||| flag
+                   member this.ToggleFlag flag = this.Mode ^^^ flag
 
     let parse (input : string) = Seq.toList input
 
-    let hasFlag state flag = state.Mode &&& flag = flag
-    let setFlag mode flag = mode ||| flag
-    let toggleFlag mode flag = mode ^^^ flag
+    let (|Cancel|_|) ((state : State, _)) = 
+        if state.HasFlag Mode.Cancel then Some() else None
+
+    let (|CancelNextCharacter|_|) ((state : State, c : char)) =
+        if c = '!' && state.HasFlag Mode.Garbage then Some() else None
+
+    let (|BeginGroup|_|) ((state : State, c : char)) =
+        if c = '{' && state.HasFlag Mode.Group then Some() else None
+
+    let (|EndGroup|_|) ((state : State, c : char)) =
+        if c = '}' && state.HasFlag Mode.Group then Some() else None
+
+    let (|BeginGarbage|_|) ((state : State, c : char)) =
+        if c = '<' && state.HasFlag Mode.Group then Some() else None
+
+    let (|EndGarbage|_|) ((state : State, c : char)) =
+        if c = '>' && state.HasFlag Mode.Garbage then Some() else None
 
     let calculate (input : char list) = 
         let result = input |> List.fold 
                                 (fun state c -> 
-                                    match c with
-                                    | _ when hasFlag state Mode.Cancel ->
-                                        { state with Mode = toggleFlag state.Mode Mode.Cancel }
-                                    | '!' when hasFlag state Mode.Garbage ->
-                                        { state with Mode = setFlag state.Mode Mode.Cancel }
-                                    | '{' when hasFlag state Mode.Group -> 
+                                    match (state, c) with
+                                    | Cancel ->
+                                        { state with Mode = state.ToggleFlag Mode.Cancel }
+                                    | CancelNextCharacter ->
+                                        { state with Mode = state.SetFlag Mode.Cancel }
+                                    | BeginGroup -> 
                                         { state with Depth = state.Depth + 1 }
-                                    | '}' when hasFlag state Mode.Group -> 
+                                    | EndGroup -> 
                                         { state with Depth = state.Depth - 1; 
                                                      Score = state.Score + state.Depth }
-                                    | '<' when hasFlag state Mode.Group ->
+                                    | BeginGarbage ->
                                         { state with Mode = Mode.Garbage }
-                                    | '>' when hasFlag state Mode.Garbage ->
+                                    | EndGarbage ->
                                         { state with Mode = Mode.Group }
                                     | _ -> state) 
                                 { Score = 0; Depth = 0; Mode = Mode.Group }
